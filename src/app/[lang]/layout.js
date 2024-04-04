09910d4ebaf279a4f0e74f50ea6393e2f1a65f20
cuonggold2408 <cuonggold2408@gmail.com>
2024-04-04 11:01:01 +0700

    update

--- src/app/[lang]/layout.js
+++ src/app/[lang]/layout.js
@@ -4,18 +4,13 @@ import { getDictionary } from "../../../getDictionary";
 
 const inter = Inter({ subsets: ["latin"] });
 
-// export const metadata = {
-//   title: "F8 portfolio",
-//   description: "Xin chào, mình là F8, đây là trang cá nhân của mình.",
-// };
-
 export default async function RootLayout({ children, params }) {
   const lang = await getDictionary(params.lang);
   return (
     <html lang={params.lang} className="light" style={{ colorScheme: "light" }}>
       <head>
         <meta name="description" content={lang.description} />
-        <title>F8 portfolio</title>
+        <title>My portfolio</title>
       </head>
       <body className={inter.className}>
         <Providers>{children}</Providers>
